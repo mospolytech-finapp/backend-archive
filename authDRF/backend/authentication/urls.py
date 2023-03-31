018e19b8d4a404e9932e2ca1d11f2ad4642f7a7e
Nejern <5qub6pfp@duck.com>
2023-03-31 17:15:22 +0300

    Очистка ветки, подготовка к вынесению сервиса finance в отдельную ветку

--- authDRF/backend/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-]
--- finance/models.py
+++ finance/models.py
@@ -5,10 +5,7 @@ from django.db import models
 class Transaction(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=128, blank=True)
-    user_uuid = models.ForeignKey(
-        'authentication.CustomUser',
-        on_delete=models.CASCADE,
-    )
+    #user_uuid = models.ForeignKey('authentication.CustomUser', on_delete=models.CASCADE,)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     type = models.BooleanField()  # false - Расход / true - Поступление
     date_time = models.DateTimeField(auto_now_add=True)
