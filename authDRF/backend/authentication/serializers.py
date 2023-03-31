5158f79c4dd83a7ada0dc659c672a7b129a80898
inkogniito <120864571+inkogniito@users.noreply.github.com>
2023-03-31 15:18:13 +0300

    Delete authDRF/backend directory

--- authDRF/backend/authentication/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import CustomUser
-
-
-class CustomUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'last_name', 'first_name', 'middle_name', 'email', 'date_of_birth', 'gender']
