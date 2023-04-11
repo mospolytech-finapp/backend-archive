192997f2a3925e242c96f12513b532f92315871d
Nejern <5qub6pfp@duck.com>
2023-04-11 11:54:45 +0300

    Внесение изменений из новых релизов концепции

--- authentication/models.py
+++ authentication/models.py
@@ -21,34 +21,20 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractBaseUser):
-    GENDER_CHOICES = (
-        ('M', 'Male'),
-        ('F', 'Female'),
-        ('O', 'Other'),
-    )
-
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
     last_name = models.CharField(max_length=30)
     first_name = models.CharField(max_length=30)
     middle_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['last_name', 'first_name', 'middle_name', 'gender']
+    REQUIRED_FIELDS = ['last_name', 'first_name']
 
     objects = CustomUserManager()
 
     def __str__(self):
         return f"{self.last_name} {self.first_name} {self.middle_name}"
 
-    def get_full_name(self):
-        return f"{self.last_name} {self.first_name} {self.middle_name}"
-
-    def get_short_name(self):
-        return self.first_name
-
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,2 @@
-from django.urls import path, include
-
 urlpatterns = [
-    path('api/auth/', include('authentication.urls')),
 ]
