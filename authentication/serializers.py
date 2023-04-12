ca0c6238225b3857df95fa37b6c2e83d2a5b5839
Nejern <5qub6pfp@duck.com>
2023-04-12 11:58:52 +0300

    Привязка сериализатора пользователя к модели, записанной в настройках, включение валидатора пароля

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
-from .models import Finapp_User
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class FinappUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Finapp_User
+        model = User
         fields = [
             'email',
             'password'
@@ -15,6 +19,10 @@ class FinappUserSerializer(serializers.ModelSerializer):
         ]
         extra_kwargs = {'password': {'write_only': True}}
 
+    def validate_password(self, value):
+        validate_password(value)  # Django password validation
+        return value
+
     def create(self, validated_data):
-        user = Finapp_User.objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
         return user
