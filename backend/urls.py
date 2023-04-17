756b06405978be0a5d48171da5f5bdd8197867ca
Nejern <5qub6pfp@duck.com>
2023-04-17 16:37:46 +0300

    Добавление redoc представления документации

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import \
+        SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 
 urlpatterns = [
     # Docs
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     path('api/auth/', include('authentication.urls')),
 ]
