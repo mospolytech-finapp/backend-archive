bc58308d60d8a4a574652619d8d3e501de1be25d
si6Och4j <1@1.com>
2023-04-21 19:07:41 +0300

    Добавление локализации предупреждений валидаторов

--- backend/settings.py
+++ backend/settings.py
@@ -118,6 +118,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+#LOCALE_PATHS = (
+#    BASE_DIR / 'locale'
+#)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
