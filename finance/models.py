7d9572c145bb1257adfc9586296ca3910ea00bc5
Nejern <5qub6pfp@duck.com>
2023-04-24 19:43:12 +0300

    Исправление модели транзакции

--- finance/models.py
+++ finance/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 
 
-class Transactions(models.Model):
+class Transaction(models.Model):
     name = models.CharField(max_length=128, blank=True, null=True)
     user_id = models.ForeignKey('authentication.Finapp_User', on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False)
-    is_arrival = models.BooleanField(blank=False, null=False)  # false - Расход / true - Поступление
     date = models.DateField(auto_now_add=True, blank=False, null=False)
     time = models.TimeField(blank=True, null=True)
     description = models.CharField(max_length=128, blank=True, null=True)
-    # category = models.ForeignKey('finance.Category', blank=False, null=False, on_delete=models.DO_NOTHING)
-    category = models.IntegerField(blank=False, null=False)
+    category = models.ForeignKey('finance.Category', on_delete=models.DO_NOTHING, blank=False, null=False)
 
 
 class Category(models.Model):
