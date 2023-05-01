7ae759087676ae17963f5ecc122a261e8ec46ede
Nejern <5qub6pfp@duck.com>
2023-05-01 16:57:23 +0300

    Удаление возможности создания транзакции с категорией, не принадлежащей пользователю

--- finance/serializers.py
+++ finance/serializers.py
@@ -3,6 +3,10 @@ from .models import Transaction, Category
 
 
 class TransactionSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+    )
+
     class Meta:
         model = Transaction
         fields = [
@@ -15,6 +19,14 @@ class TransactionSerializer(serializers.ModelSerializer):
             'description',
             'category',
         ]
+
+    def get_fields(self):
+        fields = super().get_fields()
+
+        fields['category'].queryset = fields['category'].queryset.filter(
+            owner=self.context['request'].user)
+
+        return fields
 
 
 class CategorySerializer(serializers.ModelSerializer):
