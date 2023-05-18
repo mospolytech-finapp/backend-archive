ef59579ae65cc6353cf3e1c75068af9251c6932c
Nejern <5qub6pfp@duck.com>
2023-05-18 23:02:25 +0300

    Исправление валидации пользовательских данных

--- app/authentication/serializers.py
+++ app/authentication/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import get_user_model
-
 from django.core.exceptions import ValidationError
 from datetime import date
 from django.utils.translation import gettext as _
@@ -30,13 +29,10 @@ class FinappUserSerializer(serializers.ModelSerializer):
         if date_of_birth > date.today():
             raise ValidationError(
                 _("Birth date cannot be in the future."),
-                code="birthday_in_future",
+                code="date_of_birth_in_future",
             )
         return date_of_birth
 
     def create(self, validated_data):
-        if hasattr(validated_data, "date_of_birth"):
-            self.validate_date_of_birth(validated_data.date_of_birth)
-
         user = User.objects.create_user(**validated_data)
         return user
