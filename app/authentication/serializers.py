aea23969f168d186c2cbeda7053166b6c6b1d405
Pavel <lapaev0202@mail.ru>
2023-05-18 20:44:33 +0300

    Добавление валидации даты рождения

--- app/authentication/serializers.py
+++ app/authentication/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import get_user_model
 
+from django.core.exceptions import ValidationError
+from datetime import date
+from django.utils.translation import gettext as _
 
 User = get_user_model()
 
@@ -23,6 +26,17 @@ class FinappUserSerializer(serializers.ModelSerializer):
         validate_password(password)  # Django password validation
         return password
 
+    def validate_date_of_birth(self, date_of_birth):
+        if date_of_birth > date.today():
+            raise ValidationError(
+                _("Birth date cannot be in the future."),
+                code="birthday_in_future",
+            )
+        return date_of_birth
+
     def create(self, validated_data):
+        if hasattr(validated_data, "date_of_birth"):
+            self.validate_date_of_birth(validated_data.date_of_birth)
+
         user = User.objects.create_user(**validated_data)
         return user
